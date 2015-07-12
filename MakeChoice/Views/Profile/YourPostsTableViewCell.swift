1ebfa4da479edc7b5a3e61eb3d2eb732da8129f1
Mengyu Wu <mw2907@columbia.edu>
2015-07-12 14:47:15 -0400

    modify  yourPostViewControl

---
+++ MakeChoice/Views/Profile/YourPostsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  YourPostsTableViewCell.swift
+//  MakeChoice
+//
+//  Created by 吴梦宇 on 7/12/15.
+//  Copyright (c) 2015 ___mengyu wu___. All rights reserved.
+//
+
+import UIKit
+
+class YourPostsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
