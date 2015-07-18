c37df67d7ab50d9d72dd1d0b84b988462e1723dc
Mengyu Wu <mw2907@columbia.edu>
2015-07-18 17:29:22 -0400

    send message successfully

--- MakeChoice/ViewControllers/AddressBookViewController.swift
+++ MakeChoice/ViewControllers/AddressBookViewController.swift
@@ -45,8 +45,11 @@ class AddressBookViewController: UIViewController {
             // TODO: Add actiivtyIndicator
             
             self.contacts.removeAll(keepCapacity: false)
+            
             if let contacts = contacts as? [APContact] {
                 self.contacts=contacts
+                // reload the data in case that getting data later than load tableview
+                self.tableview.reloadData()
             } else if error != nil {
                 // TODO:
                 println(error)
@@ -75,19 +78,76 @@ class AddressBookViewController: UIViewController {
 
 }
 
+// MARK: tableivewDataSouce
 extension AddressBookViewController:UITableViewDataSource{
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
       return contacts.count
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
-      var cell = tableView.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
-      cell.textLabel?.text="mengyu"
+      var cell = tableView.dequeueReusableCellWithIdentifier("addressBookCell") as! UITableViewCell
+      var contact=self.contacts[indexPath.row]
+      var username="\(contact.firstName) \(contact.lastName)"
+      var email = contact.emails.first as? String
+      var phone = contact.phones.first as? String
+      cell.textLabel?.text=username
+      cell.detailTextLabel?.text = (email != nil) ? email : phone
+      cell.detailTextLabel?.textColor = UIColor.lightGrayColor()
       return cell
     }
 
 }
 
+// MARK: tableview Delegate
 extension AddressBookViewController:UITableViewDelegate{
     
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        var user=self.contacts[indexPath.row]
+        shareWithUser(user)
+       
+    }
+    
+    func shareWithUser(user: APContact){
+        
+    let phonesCount = count(user.phones)
+        
+        if phonesCount > 0 {
+            self.sendMSG(user)
+        } else {
+            println("Contact has no phone number")
+        }
+        
+        
+    }
+    
+    
+    func sendMSG(user: APContact){
+        if MFMessageComposeViewController.canSendText() {
+            var messageCompose = MFMessageComposeViewController()
+            // TODO: Use primary phone rather than all numbers
+            messageCompose.recipients = user.phones as! [String]!
+            // body: the msg content, image url
+            messageCompose.body = "content: help me vote!"
+            messageCompose.messageComposeDelegate = self
+            self.presentViewController(messageCompose, animated: true, completion: nil)
+        } else {
+           // ProgressHUD.showError("SMS cannot be sent")
+            println("MSG cannot be sent")
+        }
+
+    }
+    
+}
+
+
+// MARK: MFMessageComposeViewControllerDelegate
+extension AddressBookViewController:MFMessageComposeViewControllerDelegate{
+    func messageComposeViewController(controller: MFMessageComposeViewController!, didFinishWithResult result: MessageComposeResult){
+        if result.value == MessageComposeResultSent.value {
+            //ProgressHUD.showSuccess("Invitation SMS sent successfully")
+            println("Invitation SMS sent successfully")
+        }
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
